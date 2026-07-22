e50b6f418cd78fe0b329a9fea8af1e9cb9a26132
Hiten Chowdhary <122hiten0016@dbit.in>
2026-07-22 15:48:10 +0530

    Add Microsoft Clarity analytics

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App.jsx";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import clarity from '@microsoft/clarity'
 
-createRoot(document.getElementById("root")).render(
+clarity.init('xqdkw4ib1n')
+
+createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-  </StrictMode>
-);
+  </StrictMode>,
+)
